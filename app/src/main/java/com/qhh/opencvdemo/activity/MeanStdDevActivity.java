baa5aaee11fc48dd7538b5a88dd0706e4028231f
qinhaihang <hai449769034@qq.com>
2019-05-22 00:35:25 +0800

    增加： 1、根据灰度图像均值进行二值分割

--- app/src/main/java/com/qhh/opencvdemo/activity/MeanStdDevActivity.java
+++ app/src/main/java/com/qhh/opencvdemo/activity/MeanStdDevActivity.java
@@ -1,13 +1,16 @@
 package com.qhh.opencvdemo.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.qhh.opencvdemo.Constants;
 import com.qhh.opencvdemo.R;
 
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDouble;
@@ -15,6 +18,8 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class MeanStdDevActivity extends AppCompatActivity {
+
+    private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,7 @@ public class MeanStdDevActivity extends AppCompatActivity {
     }
 
     private void initView() {
-
+        mImage = findViewById(R.id.image);
     }
 
     public void computer(View view) {
@@ -46,5 +51,25 @@ public class MeanStdDevActivity extends AppCompatActivity {
         Log.i("qhh_opencv","mean = "+ meanArr[0]);
         Log.i("qhh_opencv","stddev = "+ stddevArr[0]);
 
+        int width = gray.cols();
+        int height = gray.rows();
+        byte[] data = new byte[width * height];
+        gray.get(0,0,data);
+        int mean = (int)meanArr[0];
+        int pv = 0;
+        for (int i = 0; i < data.length; i++) {
+            pv = data[i]&0xff;
+            if(pv > mean){
+                data[i] = (byte) 255;
+            }else {
+                data[i] = (byte) 0;
+            }
+        }
+        gray.put(0,0,data);
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(gray,bitmap);
+
+        mImage.setImageBitmap(bitmap);
     }
 }
